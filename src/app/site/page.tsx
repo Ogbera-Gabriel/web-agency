1c4b73fb7ca48bc82605e7a946c07ec45b374286
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-24 20:19:38 +0100

    Working on page

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -5,6 +5,9 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className='h-full w-full pt-36 relative flex items-center justify-center flex-col'>
+        
+      </section>
       Main Page For All
       <SignedIn>
         <UserButton />
